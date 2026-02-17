5151d4e718b0214b9096f30b702c84407b3cae02
Marisa Chanthong <santa@Santas-MacBook.local>
2026-02-17 17:38:23 +0700

    implement PUT, DELETE

---
+++ services/student_service.go
@@ -0,0 +1,47 @@
+package services
+
+import (
+	"errors"
+
+	"example.com/student-api/models"
+	"example.com/student-api/repositories"
+)
+
+type StudentService struct {
+	Repo *repositories.StudentRepository
+}
+
+func (s *StudentService) GetStudents() ([]models.Student, error) {
+	return s.Repo.GetAll()
+}
+
+func (s *StudentService) GetStudentByID(id string) (*models.Student, error) {
+	return s.Repo.GetByID(id)
+}
+
+func (s *StudentService) CreateStudent(student models.Student) error {
+	if student.Id == "" {
+		return errors.New("id is required")
+	}
+	if student.Name == "" {
+		return errors.New("name is required")
+	}
+	if student.GPA < 0.0 || student.GPA > 4.0 {
+		return errors.New("gpa must be between 0.00 and 4.00")
+	}
+	return s.Repo.Create(student)
+}
+
+func (s *StudentService) UpdateStudent(id string, student models.Student) error {
+	if student.Name == "" {
+		return errors.New("name is required")
+	}
+	if student.GPA < 0.0 || student.GPA > 4.0 {
+		return errors.New("gpa must be between 0.00 and 4.00")
+	}
+	return s.Repo.Update(id, student)
+}
+
+func (s *StudentService) DeleteStudent(id string) error {
+	return s.Repo.Delete(id)
+}
